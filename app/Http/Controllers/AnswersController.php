b4741764ee74942e2fd8904bd094b88d01ceca82
Avinash Dengani <avinashdengani123@gmail.com>
2021-09-23 00:38:01 +0530

    Added functionality to store, update and destory method in AnswersController

--- app/Http/Controllers/AnswersController.php
+++ app/Http/Controllers/AnswersController.php
@@ -2,84 +2,54 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Answer\UpdateAnswerRequest;
 use App\Models\Answer;
-use Illuminate\Http\Request;
+use App\Models\Question;
+use CreateAnswersTable;
 
 class AnswersController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+    public function store(Question $question, CreateAnswersTable $request)
     {
-        //
+        $body = $request->body;
+        $body = explode("<div>", $body)[1];
+        $body = explode("</div>", $body)[0];
+
+        $question->answers()->create([
+            'body' => $body,
+            'user_id' => auth()->id()
+        ]);
+        session()->flash('success', 'Your answer submitted successfully!');
+        return redirect(route('questions.show', $question->slug));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
+    public function edit(Question $question, Answer $answer)
     {
-        //
+        $this->authorize('update', $answer);
+        return view('layouts.Q&A.Answer.edit', compact(['question', 'answer']));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function update(UpdateAnswerRequest $request, Question $question, Answer $answer)
     {
-        //
+        $this->authorize('update', $answer);
+        $body = $request->body;
+        $body = explode("<div>", $body)[1];
+        $body = explode("</div>", $body)[0];
+
+        $answer->update([
+            'body' => $body,
+        ]);
+        session()->flash('success', 'Your answer was updated successfully!');
+        return redirect(route('questions.show', $question->slug));
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Answer  $answer
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Answer $answer)
+    public function destroy(Question $question, Answer $answer)
     {
-        //
+        $this->authorize('delete', $answer);
+        $answer->delete();
+
+        session()->flash('success', 'Your answer was deleted successfully!');
+        return redirect()->back();
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Answer  $answer
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Answer $answer)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Answer  $answer
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Answer $answer)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Answer  $answer
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Answer $answer)
-    {
-        //
-    }
 }
