db5d3d85066bdf4bcaa3b125f7fa270cf91c5528
Chirag Mahajan <chiragmahajan3101@gmail.com>
2021-09-23 00:57:59 +0530

    getters for favorite count style and isfavourite question also established relationship for votes and views

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -22,12 +22,29 @@ class Question extends Model
         return $this->created_at->diffForHumans();
     }
 
+    public function getFavoritesCountAttribute()
+    {
+        return $this->views()->where('is_favorite', 1)->count();
+    }
+    public function getIsFavoriteAttribute()
+    {
+        return $this->favoritesUserId()->where('is_favorite', 1)->count() > 0;
+    }
+    public function getFavoriteStyleAttribute()
+    {
+        if ($this->getIsFavoriteAttribute()) {
+            return 'text-danger';
+        }
+        return 'text-black-50';
+    }
+
     //MUTATORS
     public function setTitleAttribute(String $title)
     {
         $this->attributes['title'] = $title;
         $this->attributes['slug'] = Str::slug($title);
     }
+
 
     /**
      * SCOPES
@@ -41,6 +58,11 @@ class Question extends Model
         return $query;
     }
 
+    public function favoritesUserId()
+    {
+        return $this->views()->where('user_id', '=', auth()->id());
+    }
+
     //Relationships
     public function owner()
     {
@@ -49,5 +71,13 @@ class Question extends Model
     public function answers()
     {
         return $this->hasMany(Answer::class);
+    }
+    public function votes()
+    {
+        return $this->morphToMany(User::class, 'vote')->withTimestamps();
+    }
+    public function views()
+    {
+        return $this->belongsToMany(User::class)->withTimestamps();
     }
 }
