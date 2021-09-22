1dddbd3035384d1a3206dfdbbb47c6db38277210
Chirag Mahajan <chiragmahajan3101@gmail.com>
2021-09-22 17:52:50 +0530

    Updated columns in migration for users and established DB relationships also new const for login is mentioned

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,17 +11,20 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+    const USER_TEACHER = 0;
+    const USER_ADMIN = 1;
+    const USER_STUDENT = 2;
+    const STUDENT_BRANCH_AI = 3;
+    const STUDENT_BRANCH_CS = 4;
+    const STUDENT_BRANCH_DS = 5;
+    const STUDENT_BRANCH_IT = 6;
 
     /**
      * The attributes that are mass assignable.
      *
      * @var string[]
      */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $guarded = ['id'];
 
     /**
      * The attributes that should be hidden for serialization.
@@ -31,6 +34,7 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'verification_token',
     ];
 
     /**
@@ -41,4 +45,20 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    // HELPER FUNCTIONS
+    public function isAdmin()
+    {
+        return $this->role === self::USER_ADMIN;
+    }
+
+    public function isTeacher()
+    {
+        return $this->role === self::USER_TEACHER;
+    }
+
+    public function isStudent()
+    {
+        return $this->role === self::USER_STUDENT;
+    }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,12 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('branch')->nullable();
+            $table->string('image')->nullable();
+            $table->string('verification_id');
+            $table->string('verification_token')->nullable();
+            $table->unsignedTinyInteger('role')->default(0);
+            $table->unsignedTinyInteger('approval_status')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
