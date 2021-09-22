27d212f117207eeb08c86ca2be77eff646107d7b
Avinash Dengani <avinashdengani123@gmail.com>
2021-09-23 03:05:20 +0530

    Created thoughts using Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Answer;
 use App\Models\Question;
+use App\Models\Thought;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -58,8 +59,10 @@ class DatabaseSeeder extends Seeder
             'remember_token' => Str::random(10),
         ]);
 
+        //TESTIMONIAL MESSAGES
         $GLOBALS['testMessages'] = ["It is very easy to learn and if we have any query they provide us with a discussion forum.", "I just finished the course, still following the steps that webacquire is instructing, still need to learn a lot more :)", "I like the contents in average, test are very easy and contents are easy to learn and understand", "I started with the baiscs, completed frontend development course and gained so much knowledge through various instructors."];
 
+        //USERS, QUESTIONS and ANSWERS
         User::factory(10)->create()->each(function (User $user) {
             if ($user->role) {
                 $user->update([
@@ -78,11 +81,26 @@ class DatabaseSeeder extends Seeder
                 });
         });
 
+        //Updating 3 teachers aprroval_status.
         $approvedTeacher = User::where('role', 0)->inRandomOrder()->limit(3)->get();
         foreach ($approvedTeacher as $teacher) {
             $teacher->update([
                 'approval_status' => 1
             ]);
         }
+
+        //THOUGHTS
+        $thoughts = ["Store windows are like landing pages on the website", "If You Think Math is Hard Try Web Design", "Copy and paste is a design error", "It’s harder to read code than to write it", "Code never lies; comments sometimes do.", "Ruby is rubbish! PHP is phpantastic!", "Java is to JavaScript what car is to Carpet.", "Design is not just what it looks like and feels like. Design is how it works", "Websites should look good from the inside and out", "Simplicity is the soul of efficiency", "Talk is cheap. Show me the code"];
+
+        $GLOBALS['authors'] = ["Angela Ahrendts", "Pixxelznet", "Steve McConnell", "Joel Spolsky", "Ron Jeffries", "Nikita Popov", "Chris Heilmann", "Steve Jobs", "Paul Cookson", "Austin Freeman", "Linus Torvalds"];
+
+        $i = 0;
+        foreach($thoughts as $thought) {
+            Thought::create([
+                'name' => $GLOBALS['authors'][$i],
+                'description' => $thought
+            ]);
+            $i++;
+        };
     }
 }
