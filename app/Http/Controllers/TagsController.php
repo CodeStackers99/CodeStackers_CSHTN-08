bb058af7b826b4dc75fa8f0c5cc137003ed8887c
Diksha Chaudhari <dikshapchaudhari18@gmail.com>
2021-09-23 20:21:27 +0530

    completed methods for index create and storing of tags and fixed namespace bugs

--- app/Http/Controllers/TagsController.php
+++ app/Http/Controllers/TagsController.php
@@ -2,82 +2,47 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\Tag\CreateTagRequest;
+use App\Http\Requests\Tag\UpdateTagRequest;
 use App\Models\Tag;
 use Illuminate\Http\Request;
 
 class TagsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+        $this->authorize('viewAny', Tag::class);
+        $tags = Tag::latest('updated_at')->search()->paginate(20);
+        return view('layouts.Tag.index', compact(['tags']));
     }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
-        //
+        $this->authorize('create', Tag::class);
+        return view('layouts.Tag.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function store(CreateTagRequest $request)
     {
-        //
+        $this->authorize('create', Tag::class);
+
+        Tag::create([
+            'name' => $request->name,
+            'user_id' => auth()->user()->id,
+        ]);
+        session()->flash('success', 'Tag created successfully.');
+        return redirect(route('tags.index'));
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Tag  $tag
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Tag $tag)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Tag  $tag
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Tag $tag)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Tag  $tag
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Tag $tag)
+    public function update(UpdateTagRequest $request, Tag $tag)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Tag  $tag
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Tag $tag)
     {
         //
--- app/Http/Requests/Tag/CreateTagRequest.php
+++ app/Http/Requests/Tag/CreateTagRequest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Requests\Tags;
+namespace App\Http\Requests\Tag;
 
 use Illuminate\Foundation\Http\FormRequest;
 
