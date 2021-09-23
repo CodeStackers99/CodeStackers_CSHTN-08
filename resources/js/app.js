647180a7276b96edd38533bb335cd568528490ac
Avinash Dengani <avinashdengani123@gmail.com>
2021-09-23 23:47:00 +0530

    Installed chart.js dependency using npm

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,3 +2,4 @@ require('./bootstrap');
 require('owl.carousel/dist/owl.carousel');
 require('jquery-smooth-scroll/jquery.smooth-scroll');
 require('waypoints/lib/jquery.waypoints');
+require('chart.js/dist/chart');
