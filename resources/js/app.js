505b6ffcd723e35cdb1dd26a3677fa6efd0f5e97
Avinash Dengani <avinashdengani123@gmail.com>
2021-09-24 01:12:05 +0530

    Installed video.js plugin to display videos smoothly and used js and css file of video.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,3 +3,4 @@ require('owl.carousel/dist/owl.carousel');
 require('jquery-smooth-scroll/jquery.smooth-scroll');
 require('waypoints/lib/jquery.waypoints');
 require('chart.js/dist/chart');
+require('video.js/dist/video')
