4fd88d26df0960d711f54b335d5e4113972287fe
Chirag Mahajan <chiragmahajan3101@gmail.com>
2021-09-23 10:57:13 +0530

    completed controller to allow editing, updating and destroying of courses for admin

--- app/Http/Controllers/CoursesController.php
+++ app/Http/Controllers/CoursesController.php
@@ -31,7 +31,7 @@ class CoursesController extends Controller
         $rules = [
             'name' => 'required|max:40|unique:courses',
             'description' => 'required',
-            'image'=> 'required|image|mimes:jpeg,png,jpg|max:512'
+            'image' => 'required|image|mimes:jpeg,png,jpg|max:512'
         ];
         $this->validate($request, $rules);
 
@@ -56,16 +56,45 @@ class CoursesController extends Controller
 
     public function edit(Course $course)
     {
-        //
+        $this->authorize('update', $course);
+        return view('layouts.Course.edit', compact('course'));
     }
 
     public function update(Course $course, Request $request)
     {
-        //
+        $this->authorize('update', $course);
+
+        $rules = [
+            'name' => 'required|max:40|unique:courses,name,' . $course->id,
+            'description' => 'required',
+            'image' => 'image|mimes:jpeg,png,jpg|max:512',
+        ];
+
+        $this->validate($request, $rules);
+
+        if ($request->hasFile('image')) {
+            $course->deleteImage();
+            $image = $request->image->store('images/courses');
+        } else {
+            $image = $course->image;
+        }
+
+        $course->update([
+            'name' => $request->name,
+            'description' => $request->description,
+            'image' => $image
+        ]);
+
+        session()->flash('success', 'Course has been edited Successfully!');
+        return redirect(route('courses.index'));
     }
 
     public function destroy(Course $course)
     {
-        //
+        $this->authorize('delete', $course);
+        $course->deleteImage();
+        $course->delete();
+
+        return redirect(route('courses.index'));
     }
 }
