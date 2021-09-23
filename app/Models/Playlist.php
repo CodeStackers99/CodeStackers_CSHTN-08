5f8e1a0cbd4c5439165238ed7a53d61dd8be20f9
Chirag Mahajan <chiragmahajan3101@gmail.com>
2021-09-23 12:02:47 +0530

    created playlist migration and updated its required columns also established DB relationships and general helper and getter methods

---
+++ app/Models/Playlist.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+
+class Playlist extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id'];
+
+    //HELPERS
+    public function deleteImage()
+    {
+        Storage::delete($this->display_image);
+    }
+
+    //Getters
+    public function getImagePathAttribute()
+    {
+        return "storage/" . $this->display_image;
+    }
+    public function getUrlAttribute()
+    {
+        return $this->subCourse->url . "/playlists/{$this->slug}";
+    }
+
+    //MUTATORS
+    public function setTitleAttribute(String $title)
+    {
+        $this->attributes['title'] = $title;
+        $this->attributes['slug'] = Str::slug($title);
+    }
+    //RELATIONSHIPS
+    public function owner()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+    public function subcourse()
+    {
+        return $this->belongsTo(SubCourse::class, 'sub_course_id');
+    }
+
+    //SCOPES
+    public function scopeSearch($query)
+    {
+        $search = request('search');
+        if ($search) {
+            return $query->where("title", "like", "%$search%");
+        }
+        return $query;
+    }
+}
---
+++ database/migrations/2021_09_23_062659_create_playlists_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlaylistsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('playlists', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('description');
+            $table->string('display_image');
+            $table->unsignedInteger('hours');
+            $table->unsignedBigInteger('sub_course_id');
+            $table->unsignedBigInteger('user_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+
+            $table->foreign('sub_course_id')
+                ->references('id')
+                ->on('sub_courses')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('playlists');
+    }
+}
