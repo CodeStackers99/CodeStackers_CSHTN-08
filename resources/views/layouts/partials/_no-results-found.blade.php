4a604ed29ab5fb168ad536cfce5ccfe30335bbc7
Niharika Joshi <niharika.joshi062001@gmail.com>
2021-09-24 00:14:42 +0530

    Created no results found partial layout file.

---
+++ resources/views/layouts/partials/_no-results-found.blade.php
@@ -0,0 +1,6 @@
+<div class=" section d-flex flex-row justify-content-center align-items-center section-divider">
+    <div class="text">
+        <h2><strong class="text-hblack text-capitalize text-center sub-heading">No Results found :(</strong></h2>
+    </div>
+    <img src="{{asset('images/others/not_found.png')}}" alt="No-Results-Found" class="no-result-found-img">
+</div>
