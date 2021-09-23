d66189f1adc805e7dd7d39543157e42edf1f39ed
Avinash Dengani <avinashdengani123@gmail.com>
2021-09-23 19:47:55 +0530

    Initialized all the policies in AuthServiceProvider

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,18 @@
 
 namespace App\Providers;
 
+use App\Models\Answer;
+use App\Models\Course;
+use App\Models\Playlist;
+use App\Models\Question;
+use App\Models\SubCourse;
+use App\Models\Tag;
+use App\Policies\AnswerPolicy;
+use App\Policies\CoursePolicy;
+use App\Policies\PlaylistPolicy;
+use App\Policies\QuestionPolicy;
+use App\Policies\SubCoursePolicy;
+use App\Policies\TagPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -13,7 +25,13 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+
+        Question::class => QuestionPolicy::class,
+        Answer::class => AnswerPolicy::class,
+        Course::class => CoursePolicy::class,
+        SubCourse::class => SubCoursePolicy::class,
+        Playlist::class => PlaylistPolicy::class,
+        Tag::class => TagPolicy::class
     ];
 
     /**
