e97175151bd78f16d099c2ea38ae45cdc7e8e520
Chirag Mahajan <chiragmahajan3101@gmail.com>
2021-09-24 00:01:47 +0530

    added variables storing the analysis of enrolled playlists and passed to dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -20,30 +20,36 @@ class HomeController extends Controller
     public function dashboard()
     {
         $recentlyViewedVideo = auth()
-                            ->user()
-                            ->videos()
-                            ->latest('pivot_updated_at')
-                            ->where('is_watch_later', 0)
-                            ->limit(10)
-                            ->get();
+            ->user()
+            ->videos()
+            ->latest('pivot_updated_at')
+            ->where('is_watch_later', 0)
+            ->limit(10)
+            ->get();
 
         $topPlaylists = Video::orderBy('likes_count', 'DESC')
-                        ->with('playlist')
-                        ->get()
-                        ->pluck('playlist')
-                        ->unique();
+            ->with('playlist')
+            ->get()
+            ->pluck('playlist')
+            ->unique();
 
         $watchLaterVideos = auth()
-                            ->user()
-                            ->videos()
-                            ->where('is_watch_later', 1)
-                            ->get();
+            ->user()
+            ->videos()
+            ->where('is_watch_later', 1)
+            ->get();
 
         $courses = Course::all();
 
         $thought = Thought::inRandomOrder()->first();
 
+        $enrolledInComplete = auth()->user()->enrolledPlaylist()->where('is_completed', 0)->whereBetween('completion_deadline', [now(), Carbon::today()->addDays(7)])->get();
 
-        return view('layouts.dashboard', compact(['recentlyViewedVideo', 'topPlaylists', 'watchLaterVideos', 'courses', 'thought']));
+        $enrollAnalysis['total'] = auth()->user()->enrolledPlaylist()->count();
+        $enrollAnalysis['completed'] = auth()->user()->enrolledPlaylist()->where('is_completed', 1)->count();
+        $enrollAnalysis['in_complete'] = auth()->user()->enrolledPlaylist()->where('is_completed', 0)->where('completion_deadline', '<=', now())->count();
+        $enrollAnalysis['can_complete'] = $enrollAnalysis['total'] - $enrollAnalysis['completed'] - $enrollAnalysis['in_complete'];
+
+        return view('layouts.dashboard', compact(['recentlyViewedVideo', 'topPlaylists', 'watchLaterVideos', 'courses', 'thought', 'enrollAnalysis', 'enrolledInComplete']));
     }
 }
