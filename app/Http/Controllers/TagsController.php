ea562c8aa7e247edd3f1cf6f2c2e5bba6cf638c2
Roshni Kukreja <72001129+roshnikukreja@users.noreply.github.com>
2021-09-23 20:38:29 +0530

    Completed methods for update, edit and authorized deleting of tags

--- app/Http/Controllers/TagsController.php
+++ app/Http/Controllers/TagsController.php
@@ -35,16 +35,31 @@ class TagsController extends Controller
 
     public function edit(Tag $tag)
     {
-        //
+        $this->authorize('update', $tag);
+        return view('layouts.Tag.edit', compact(['tag']));
     }
 
     public function update(UpdateTagRequest $request, Tag $tag)
     {
-        //
+        $this->authorize('update', $tag);
+        $tag->name = $request->name;
+        $tag->save();
+        session()->flash('success', 'Tag updated Successfully.');
+        return redirect(route('tags.index'));
     }
 
     public function destroy(Tag $tag)
     {
-        //
+        $this->authorize('delete', $tag);
+        
+
+        if($tag->videos->count() > 0) {
+            session()->flash('error', 'Tag cannot be deleted as it contains many videos.');
+            return redirect(route('tags.index'));
+        }
+        $tag->delete();
+
+        session()->flash('success', 'Tag has been deleted successfully.');
+        return redirect(route('tags.index'));
     }
 }
