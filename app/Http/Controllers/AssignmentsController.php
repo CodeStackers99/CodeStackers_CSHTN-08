2f1b44f4550f05f9d0b857da8c63a47cbf4ad688
Chirag Mahajan <chiragmahajan3101@gmail.com>
2021-09-25 07:59:02 +0530

    methods to return quiz report, quiz show, check and analyze

--- app/Http/Controllers/AssignmentsController.php
+++ app/Http/Controllers/AssignmentsController.php
@@ -7,6 +7,8 @@ use App\Models\AssignmentAnswer;
 use App\Models\AssignmentEntry;
 use App\Models\Course;
 use App\Models\Playlist;
+use App\Models\Quiz;
+use App\Models\QuizEntry;
 use App\Models\SubCourse;
 use App\Models\Video;
 use Illuminate\Http\Request;
@@ -69,7 +71,13 @@ class AssignmentsController extends Controller
         session()->flash('success', 'Assignment has been submitted succesfully. Your score is ' . $marks . '/' . $noOfQuestions . 'Thank you');
         return redirect(route('courses.subcourses.playlists.videos.show', [$course->slug, $subCourse->slug, $playlist->slug, $video->slug]));
     }
-    public function quiz(Assignment $assignment)
+    public function quiz()
+    {
+        $quizUndertaken = Quiz::where('user_id', auth()->id())->get();
+        return view('layouts.Quiz.index', compact(['quizUndertaken']));
+    }
+
+    public function quizStart()
     {
         if (auth()->user()->role == 2) {
             $assignmentQuestions = [];
@@ -78,10 +86,60 @@ class AssignmentsController extends Controller
                     $assignmentQuestions[] = $question;
                 }
             }
-            dd($assignmentQuestions);
+            return view('layouts.Quiz.show', compact(['assignment', 'assignmentQuestions']));
         }
         session()->flash('error', 'Only Students can Take Quiz.');
         return redirect()->back();
+    }
+
+    public function Quizcheck(Request $request)
+    {
+        $quiz = Quiz::create([
+            'user_id' => auth()->id(),
+        ]);
+        $array = $request->toArray();
+        $lastKey = array_key_last($array);
+        $count = (int)explode('id', $lastKey)[1];
+        $marks = 0;
+        for ($i = 1; $i <= $count; $i++) {
+            $result = 0;
+            if ($request->input('answer_id' . $i)) {
+                $ans_id = $request->input('answer_id' . $i);
+            } else {
+                $ans_id = 0;
+            }
+            $ans = AssignmentAnswer::find($ans_id);
+            if ($ans) {
+                if ($ans->is_correct) {
+                    $result = 1;
+                    $marks++;
+                }
+                QuizEntry::create([
+                    'user_id' => auth()->user()->id,
+                    'assignment_question_id' => $request->input('question_id' . $i),
+                    'assignment_answer_id' => $ans_id,
+                    'result' => $result,
+                    'quiz_id' => $quiz->id,
+                ]);
+            }
+        }
+        $quiz->update([
+            'marks_obtained' => $marks,
+            'total_questions' => $count,
+        ]);
+        session()->flash('success', 'Quiz has been submitted succesfully. Your score is ' . $marks . '/' . $count . 'Thank you');
+        return redirect(route('quiz'));
+    }
+
+    public function quizAnalyze(Quiz $quiz)
+    {
+        if ($quiz->user_id === auth()->id()) {
+            $questionAnswers = $quiz->quizEntries()->get();
+            return view('layouts.Quiz.analysis', compact(['quiz', 'questionAnswers']));
+        } else {
+            session()->flash('error', 'You Can Only View Your Quiz Analysis');
+            return redirect()->back();
+        }
     }
 
     public function analyze()
